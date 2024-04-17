e58a157e635708112011284902f4efce1ffd5cd8
dev-nii-nartey <ictkemba@gmail.com>
2024-04-17 01:11:19 +0000

    feat: add candidate storing and fetching controllers

--- src/app/controllers/admin-controller.ts
+++ src/app/controllers/admin-controller.ts
@@ -1,40 +1,54 @@
 import { NextFunction, Request, Response, response } from "express";
 import { matchedData, validationResult } from "express-validator";
-import UserRepository from "../models/user-model";
-import { hashSync } from "bcryptjs";
-import { Role } from "@prisma/client";
 import { HttpException } from "../exceptions/exception";
 import { success_code, unprocessableEntity } from "../configs/status-code";
 import { JsonOutput } from "../middlewares/json.response-middleware";
-import { userRole } from "../utils/url.extractor";
+import CandidateRepository from "../models/candidate-model";
+import Database from "../configs/db-config";
 
 export default class AdminController {
   constructor() {}
-
-  static async register(
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) {
+  static async addCandidate(request: Request, response: Response) {
     const result = validationResult(request);
     if (!result.isEmpty()) {
       throw new HttpException(unprocessableEntity, result.array());
     }
-    const { name, email, password } = matchedData(request);
-    const role =  userRole(request.url);
-    const userId = await UserRepository.customUserId(role);
-    const userPayload = new UserRepository(
-      email,
-      password,
-      userId,
-      Role.REGISTRAR,
-      name
+    const {
+      name,
+      nssNumber,
+      district,
+      portfolio,
+      institutionAttended,
+      qualification,
+      posting,
+      position,
+      contact,
+    } = matchedData(request);
+    const candidate = new CandidateRepository(
+      name,
+      nssNumber,
+      institutionAttended,
+      qualification,
+      posting,
+      position,
+      contact,
+      portfolio,
+      district
     );
-    const newUser = await userPayload.add();
+    const newCandidate = await candidate.add();
     const responseData = {
-      message: `User account created successfully`,
-      details: { newUser },
+      message: `Candidate  added successfully`,
+      details: { newCandidate },
     };
     return response.status(success_code).json(new JsonOutput(responseData));
   }
+
+  // static async fetchAllCandidates(request: Request, response: Response) {
+  //   const candidates = await CandidateRepository.fetchCandidates();
+  //   const responseData = {
+  //     message: `Candidate  added successfully`,
+  //     details: { candidates },
+  //   };
+  //   return response.status(success_code).json(new JsonOutput(responseData));
+  // }
 }
